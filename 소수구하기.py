7e8317a4f9385291aaa3b8d1ea4efcbe232a1539
Hansm-a <48609198+Hansm-a@users.noreply.github.com>
2019-03-16 13:21:06 +0900

    Update 소수구하기.py

--- 소수구하기.py
+++ 소수구하기.py
@@ -1,15 +1,13 @@
-int(inputcount)
-int(count)
-int(primecount)
-int(i)
-int(j)
+inputcount=0
+count=0
+primecount=0
+i=0
+j=0
 
 inputcount = input('몇번째 소수를 출력할까요? : ')
 
-for i in range(1,999999999999)
-    i=i+1
-    for j in range(1,i)
-        j=j+1
+for i in range(1,999999999999) :
+    for j in range(1,i) :
         if count>2 :
             break
         if i%j==0 :
@@ -19,6 +17,5 @@ for i in range(1,999999999999)
     if inputcount == primecount :
         break
     count = 0
-
 
 print(inputcount + "번째 소수 : " + i)
