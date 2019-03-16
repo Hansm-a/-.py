636a040909157e6aac5e4c23e32766f794c6e5c8
Hansm-a <48609198+Hansm-a@users.noreply.github.com>
2019-03-16 13:52:18 +0900

    Update 소수구하기.py

--- 소수구하기.py
+++ 소수구하기.py
@@ -1,23 +1,23 @@
-inputcount=0
-count=0
-primecount=0
-i=0
-j=0
+inputcount=0                                            #사용자가 입력한 숫자를 저장할 변수
+count=0                                                 #약수의 개수를 카운팅할 변수
+primecount=0                                            #input까지의 소수개수를 카운팅할 변수
+i=0                                                     #for문과 출력에 쓰일 변수
+j=0                                                     #for문에 쓰일 변수    
 
-inputcount = input('몇번째 소수를 출력할까요? : ')
+inputcount = input('몇번째 소수를 출력할까요? : ')        #입력받은값 inputcount에 저장
 
-for i in range(1,999999999999) :
-    i=i+1
-    for j in range(1,i) :
-        j=j+1
-        if count>2 :
-            break
-        if i%j==0 :
+for i in range(1,999999999999) :                        #main for문 시작
+    i=i+1                                               #i 증가
+    for j in range(1,i) :                               #약수개수 counting for문 시작
+        j=j+1                                           #j 증가
+        if count>2 :                                    #약수개수가 2개를 넘으면 break
+            break                                       
+        if i%j==0 :                                     #j로 나누어 떨어지는 i값이 있으면 i의 약수개수 1 증가                                                                                       
             count+=1
-    if count==2 : 
-        primecount+=1
-    if inputcount == primecount :
+    if count==2 :                                       #counting for문에서 count가 2로 나왔을경우 소수로 판단하여   
+        primecount+=1                                   #primecount를 1증가
+    if inputcount == primecount :                       #inputcount 값과 primecount값이 같아지면 main for 문 탈출
         break
-    count = 0
-
-print(inputcount,"번째 소수 : ",i)
+    count = 0                                           #count 초기화 한후 main for 문 반복
+ 
+print(inputcount,"번째 소수 : ",i)                       #결과값 
